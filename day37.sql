d7efb8b2c493f5fe63314009ccacc831f08c443e
Ananya Gupta <50453232+ananya0504@users.noreply.github.com>
2020-10-22 03:01:15 +0530

    Add files via upload

---
+++ day37.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Jobs (
+  	EmpName varchar (20),
+  	Job varchar (20),
+  	Salary number(6)
+  );
+
+insert into Jobs Values('James Halpert', 'Salesman', 47000);
+insert into Jobs Values('Michael Scott', 'Manager', 80000);
+insert into Jobs Values('Dwight Schrite', 'Salesman', 82000);
+insert into Jobs Values('Stanley Hudson', 'Salesman', 92000);
+insert into Jobs Values('Angela Markel', 'Accountant', 47000);
+insert into Jobs Values('Kelly Kapoor', 'Customer Services', 23000);
+insert into Jobs Values('Kevin Malone', 'Accountant', 33000);
+insert into Jobs Values('Toby Flendersonn', 'Human Resources', 45000);
+insert into Jobs Values('Ryan Howard', 'Temp', 11000);
+insert into Jobs Values('Oscar Martinez', 'Accountant', 43000);
+
+SELECT  * FROM 
+    (
+    SELECT * FROM Jobs 
+    ORDER BY Salary desc
+    )
+WHERE rownum <= 3
+ORDER BY Salary desc ;
